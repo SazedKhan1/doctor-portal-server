46b4ca3ced027d84a270b2b90a86e0d778371aab
Md: Sazed Khan <manthedark233@gmail.com>
2023-10-18 23:34:38 +0600

    added file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 const app = express()
 
@@ -8,10 +11,126 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vi0yhuk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// jwt token varrify fuction 
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('doctors-portal').collection('appointment-collections')
+        const bookingCollections = client.db('doctors-portal').collection('bookings')
+        const userCollection = client.db('doctors-portal').collection('users')
+
+
+        app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const query = {}
+            const options = await appointmentOptionCollection.find(query).toArray()
+            const bookingQuery = { appointmentDate: date }
+            const alreadyBooked = await bookingCollections.find(bookingQuery).toArray()
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
+                const bookedSlots = optionBooked.map(book => book.slot)
+                const remaingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+                option.slots = remaingSlots
+            })
+            res.send(options)
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            console.log(user)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
+            const query = { email: email };
+            const bookings = await bookingCollections.find(query).toArray();
+            res.send(bookings);
+        })
+
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+            const alreadyBooked = await bookingCollections.find(query).toArray()
+            if (alreadyBooked.length) {
+                const message = `You have already booked this on ${booking.appointmentDate}`
+                return res.send({ acknowledge: false, message })
+            }
+            const result = await bookingCollections.insertOne(booking)
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+
+        })
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', async (req, res) => {
     res.send("Doctors portal running on the port")
 })
-
 
 
 app.listen(port, () => console.log(`Doctors portal server running on the port ${port}`))
